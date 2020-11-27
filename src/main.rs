a5dbfa3fb3dc1e8e816b77f8eb5f1ec91e64e07d
PhotonQuantum <self@lightquantum.me>
2020-11-27 11:37:15 +0800

    feat: specify file to parse

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,13 @@ use std::io::Read;
 use crate::parser::parse;
 use crate::transpiler::transpile;
 use std::process::exit;
+use std::env::args;
 
 mod parser;
 mod transpiler;
 
 fn main() {
-    let mut file = File::open("run.l").unwrap();
+    let mut file = File::open(args().nth(1).unwrap()).unwrap();
     let mut buffer = String::new();
     file.read_to_string(&mut buffer).unwrap();
     match parse(&buffer) {
